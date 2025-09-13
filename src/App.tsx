6556424229b9e18b8917e846fb9db9836460e08e
Yashas-qbreaker <yashasyash485@gmail.com>
2025-09-13 15:33:11 +0530

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/urbannest/">
         <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="/search" element={<SearchResults />} />
